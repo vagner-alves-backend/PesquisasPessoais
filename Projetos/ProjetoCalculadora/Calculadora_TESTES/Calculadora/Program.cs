7ca371753778f42ca8c6a2e5a66dd8fb028fc411
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-04-29 21:37:21 -0300

    Nova pasta de projeto Calculadora, projeto de testes. Calculadora_TESTE - Inicio primeiros comandos de escolha para os usuario.

---
+++ Projetos/ProjetoCalculadora/Calculadora_TESTES/Calculadora/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear ();
+
+int option = 0;
+string? option_text = "";
+bool _continue = true;
+
+do
+{
+    Console.WriteLine (
+        "Qual operação deseja realizar?\n"+
+        "\t1 - Adção\n"+
+        "\t2 - Subtração\n"+
+        "\t3 - Multiplicação\n"+
+        "\t4 - Divisão\n\n"
+    );
+
+    option_text = Console.ReadLine ();
+    Console.Clear ();
+    if (int.TryParse (option_text, out option))
+    {
+        option_text = option switch
+        {
+            1 => "Adção",
+            2 => "Subtração",
+            3 => "Multiplicação",
+            4 => "Divisão",
+            _ => "Opção não emcontrada."
+        };
+        Console.WriteLine ($"\t{option} - {option_text}");
+        _continue = false;
+    } else
+    {
+        Console.WriteLine ("[Erro]: Valor inrregular, favor informe uma das opções presentes abaixo.");
+    }
+} while (_continue);
